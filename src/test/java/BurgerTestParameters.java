232003d595df3c89edfceb6212a8af0c491a0888
David <arsdavkin@mail.ru>
2024-12-10 14:14:37 +0400

    changes

--- src/test/java/BurgerTestParameters.java
+++ src/test/java/BurgerTestParameters.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-//
+////
 @RunWith(Parameterized.class)
 public class BurgerTestParameters {
 
